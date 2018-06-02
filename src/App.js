047e1df7bcb70c5edfd618bf6a3af2393dd51f3d
Ben Willetts <bendwilletts@Bens-Air.local>
2018-06-02 14:19:47 -0400

    HomePage and ExpPage set

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 import React from 'react';
+import {Route} from "react-router-dom";
+import HomePage from "./components/pages/HomePage";
+import ExpPage from "./components/pages/ExpPage.js";
 
-const App = () => <div>Hello World</div>;
+const App = () => (
+	<div>
+		<Route path="/" exact component={HomePage} />
+		<Route path="/experience" exact component={ExpPage} />
+	</div>
+);
 
 export default App;
---
+++ src/components/pages/ExpPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+const ExpPage = () =>(
+	<div>
+		<h1>Experience Page</h1>
+	</div>
+);
+
+export default ExpPage;
+
